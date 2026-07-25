8db4ed70d8167f0b11455558ee3c5b1187582e89
edyirdaw <edyirdaw@gmail.com>
2026-07-25 09:13:39 +0200

    Adeded perplexity calculations and print outputs

--- train.py
+++ train.py
@@ -262,7 +262,10 @@ while True:
     # evaluate the loss on train/val sets and write checkpoints
     if iter_num % eval_interval == 0 and master_process:
         losses = estimate_loss()
-        print(f"step {iter_num}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+        # print(f"step {iter_num}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+        train_ppl = math.exp(losses['train'])
+        val_ppl = math.exp(losses['val'])
+        print(f"step {iter_num}: train loss {losses['train']:.4f} (ppl: {train_ppl:.2f}), val loss {losses['val']:.4f} (ppl: {val_ppl:.2f})")
         if wandb_log:
             wandb.log({
                 "iter": iter_num,
@@ -324,7 +327,9 @@ while True:
         if local_iter_num >= 5: # let the training loop settle a bit
             mfu = raw_model.estimate_mfu(batch_size * gradient_accumulation_steps, dt)
             running_mfu = mfu if running_mfu == -1.0 else 0.9*running_mfu + 0.1*mfu
-        print(f"iter {iter_num}: loss {lossf:.4f}, time {dt*1000:.2f}ms, mfu {running_mfu*100:.2f}%")
+        # print(f"iter {iter_num}: loss {lossf:.4f}, time {dt*1000:.2f}ms, mfu {running_mfu*100:.2f}%")
+        ppl = math.exp(lossf)
+        print(f"iter {iter_num}: loss {lossf:.4f} (ppl: {ppl:.2f}), time {dt*1000:.2f}ms, mfu {running_mfu*100:.2f}%")
     iter_num += 1
     local_iter_num += 1
 
