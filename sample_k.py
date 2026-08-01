19149a3b2a1b1378234b6359379ee468cdc5025e
edyirdaw <edyirdaw@gmail.com>
2026-08-01 21:22:09 +0200

    typo fix

--- sample_k.py
+++ sample_k.py
@@ -6,7 +6,7 @@ import pickle
 from contextlib import nullcontext
 import torch
 import tiktoken
-from model _k import GPTConfig, GPT
+from model_k import GPTConfig, GPT
 
 # -----------------------------------------------------------------------------
 init_from = 'resume' # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
